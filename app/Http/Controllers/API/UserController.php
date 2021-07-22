bb33163310958fb797595a799fec699c47e26884
ianahmfachriza <me.business25@gmail.com>
2021-07-22 09:08:04 +0700

    Logout API

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -112,4 +112,10 @@ class UserController extends Controller
             ], 'Update profile failed', 500);
         }
     }
+
+    public function logout(Request $request)
+    {
+        $token = $request->user()->currentAccessToken()->delete();
+        return ResponseFormatter::success($token, 'Token revoked');
+    }
 }
